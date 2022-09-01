70341f337aae127803075e0d25f116ef36efdbd5
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-31 22:11:38 -0300

    Creating main contract

---
+++ contracts/MyEpicGame.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract MyEpicGame {
+    constructor() {
+        //nao se deve usar acentos em arquivos.sol
+        console.log("Esse eh o contrato do meu jogo, bora!");
+    }
+}
