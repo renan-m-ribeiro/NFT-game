4bc206229e47c21b51832a87aa9950c18600e9cd
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-09-01 23:30:38 -0300

    Update main contract

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,12 +1,118 @@
 //SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./libraries/Base64.sol";
 
 import "hardhat/console.sol";
 
-contract MyEpicGame {
-    constructor() {
-        //nao se deve usar acentos em arquivos.sol
-        console.log("Esse eh o contrato do meu jogo, bora!");
+contract MyEpicGame is ERC721 {
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    CharacterAttributes[] defaultCharacters;
+
+    //mapping tokenId => atributos das NFTs
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    //mapping de endereço => tokenId das NFTs
+    mapping(address => uint256) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDamage
+    ) 
+
+    ERC721("Heroes", "HERO") 
+    
+    {
+        for (uint256 i = 0; i < characterNames.length; i++) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDamage[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Personagem iniciado: %s com %s de HP, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "Esta NFT da acesso ao meu jogo NFT!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strHp,
+                ', "max_value":',
+                strMaxHp,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(abi.encodePacked("data:application/json;base64,", json));
+
+        return output;
+    }
+
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+        console.log(
+            "Mintou NFT c/ tokenId %s e characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        nftHolders[msg.sender] = newItemId;
+
+        _tokenIds.increment();
     }
 }
