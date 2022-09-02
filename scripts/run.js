7272baa86909620e156f480648a1a9dc1510f135
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-09-02 17:33:29 -0300

    Update Values and Test

--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,35 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
     const gameContract = await gameContractFactory.deploy(
-      ["Kenshin", "Sanosuke", "Yahiko"],
-          [
-              "https://imgur.com/pUV8yj0",
-              "https://imgur.com/I25flmU",
-              "https://imgur.com/LTJRjlZ",
-          ],
-      [100, 200, 25], // HP values
-      [100, 50, 25] // Attack damage values
+      // Character names
+      [
+        "Kenshin",
+        "Sanosuke", 
+        "Yahiko"
+      ],
+      // Image URLs
+      [
+        "https://imgur.com/pUV8yj0", // Kenshin image
+        "https://imgur.com/I25flmU", // Sanosuke image
+        "https://imgur.com/LTJRjlZ", // Yahiko image
+      ],
+      // HP values
+      [
+        150, // Kenshin HP
+        200, // Sanosuke HP
+        100 // Yahiko HP
+      ],
+      // Attack damage values
+      [
+        100, // Kenshin attack damage 
+        50, // Sanosuke attack damage 
+        150 // Yahiko attack damage
+      ],    
+      //Boss Attributes
+      "Death Samurai", // Boss name
+      "https://imgur.com/PcDaFG8.jpg", // Boss image
+      1000, // Boss HP
+      50 // Boss Attack damage
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
@@ -16,8 +37,17 @@ const main = async () => {
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
 
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("TokenURI:", returnedTokenUri);
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    //let returnedTokenUri = await gameContract.tokenURI(1);
+    //console.log("TokenURI:", returnedTokenUri);
   };
   
   const runMain = async () => {
